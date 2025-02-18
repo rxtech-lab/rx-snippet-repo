19633127b4eb04aee7152a21007a1bff48f6691f
sirily11 <32106111+sirily11@users.noreply.github.com>
2025-02-18 15:38:50 +0800

    fix: ci

--- apps/spec-visulization/src/app/spec/[spec-file]/page.tsx
+++ apps/spec-visulization/src/app/spec/[spec-file]/page.tsx
@@ -1,16 +1,21 @@
 import * as fs from "node:fs";
-import path from "node:path";
-import Link from "next/link";
-import ClientPage from "./ClientPage";
 import yaml from "yaml";
+import path from "node:path";
+import dynamic from "next/dynamic";
+
 const specMap: { [key: string]: string } = {
-  repository: "../../spec/repository/repository.spec.yaml",
+  repository: path.join(
+    process.cwd(),
+    "../../spec/repository/repository.spec.yaml"
+  ),
 };
+
+const ClientPage = dynamic(() => import("./ClientPage"), { ssr: !!false });
 
 export default async function SpecPage({ params }: any) {
   const fileName = (await params)["spec-file"];
-  const filePath = specMap[fileName] as any;
-  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const localPath = specMap[fileName] as any;
+  const fileContent = fs.readFileSync(localPath, "utf-8");
   const parsedContent = yaml.parse(fileContent);
 
   return <ClientPage title={fileName} fileContent={parsedContent} />;
