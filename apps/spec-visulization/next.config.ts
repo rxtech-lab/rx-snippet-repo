df2ab520db59e10c3cf72dd0ad92e40066d33908
sirily11 <32106111+sirily11@users.noreply.github.com>
2025-02-18 17:14:43 +0800

    fix: copy file to the root (#1)
    * fix: add package manager field
    
    * fix: ci

--- apps/spec-visulization/next.config.ts
+++ apps/spec-visulization/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // includes files from the monorepo base two directories up
+  outputFileTracingRoot: path.join(__dirname, "../.."),
 };
 
 export default nextConfig;
